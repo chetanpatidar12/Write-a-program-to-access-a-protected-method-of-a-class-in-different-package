beb0796394f010fb02aca320df91a8952cef7f4b
chetan patidar <chetan patidar>
2017-01-07 03:35:47 +0530

    https://github.com/chetanpatidar12/Write-a-program-to-access-a-protected-method-of-a-class-in-different-package.git

---
+++ A.java
@@ -0,0 +1,28 @@
+package com.acad.basic;
+
+class C{
+public void test(){
+System.out.println("C");
+}
+}
+  
+class B extends C{
+public void test(){
+System.out.println("B");
+}
+}
+  
+public class A extends B{
+public void test(){
+	System.out.println("A");
+	super.test();
+
+}
+public static void main(String[] args) {
+
+}
+//ANSWER-f. It is not possible to invoke test() method defined in C from a method in A.
+}
+		
+	
+
